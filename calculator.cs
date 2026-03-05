d2724f622e8551d4765cd3adcc89db6dbe4b56f7
diedericksyousha-collab <diedericksyousha@gmail.com>
2026-03-05 09:27:54 +0200

    modified alc and added a bool loop

--- calculator.cs
+++ calculator.cs
@@ -6,6 +6,11 @@ namespace Program
     {
         static void Main(string [] args)
         {
+            bool isRunning;
+
+            while (isRunning = true)
+            {
+
             Console.WriteLine("Welcome to the calculator, please enter your first digit: ");
             int x = int.Parse(Console.ReadLine());
 
@@ -19,22 +24,27 @@ namespace Program
 
             if(sign == '-')
             {
-                Console.WriteLine(x - y);                
+                Console.WriteLine(x - y);
+                isRunning = true;        
             }
 
             else if(sign == '+')
             {
                 Console.WriteLine(x + y);
+                isRunning = true;
             }
 
             else if(sign == '/')
             {
                 Console.WriteLine(x / y);
+                isRunning = true;
             }
 
             else if(sign == '*')
             {
                 Console.WriteLine(x * y);
+                isRunning = true;
+            }
             }
         }
     }
