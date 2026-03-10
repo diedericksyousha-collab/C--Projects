2afcc1de10ed9243759185dd2451be02f2c41fc9
Moegamat Yousha Diedericks <diedericksyousha@gmail.com>
2026-03-10 15:43:03 +0200

    Add multiple classes and methods to clean up code

--- LoginPage.cs
+++ LoginPage.cs
@@ -2,10 +2,10 @@ using System;
 
 namespace  LoginProgram
 {
-    class LoginProgram
+    class LoginPage
     {
         static void Main(string [] args)
-        {
+        { 
             Console.WriteLine("Enter your username: ");
             string userName = Console.ReadLine();
 
@@ -15,22 +15,12 @@ namespace  LoginProgram
 
             if (userName == "Admin123" && password == "admin123")
             {
-                Console.Clear();
-                Console.WriteLine("DashBoard: ");
-                Console.WriteLine("\n1. Analytics ");
-                Console.WriteLine("\n2. Customer Details ");
-                Console.WriteLine("\n3. Stock Details ");
-                Console.WriteLine("\n4. Employees ");
+                UserInterface.Admin();
             }
 
             else if (userName == "Customer01" && password == "12345678")
             {
-                Console.Clear();
-                Console.WriteLine("Account: ");
-                Console.WriteLine("\n1. Cart ");
-                Console.WriteLine("\n2. Items ");
-                Console.WriteLine("\n3. Settings ");
-                Console.WriteLine("\n4. Payment Details ");
+                UserInterface.Customer();
             }
 
             else
@@ -38,6 +28,29 @@ namespace  LoginProgram
                 Console.Clear();
                 Console.WriteLine("Incorrect details entered, now closing application");
             }
+        }
+    }
+
+    class UserInterface
+    {
+        public static void Admin()
+        {
+            Console.Clear();
+            Console.WriteLine("DashBoard: ");
+            Console.WriteLine("\n1. Analytics ");
+            Console.WriteLine("\n2. Customer Details ");
+            Console.WriteLine("\n3. Stock Details ");
+            Console.WriteLine("\n4. Employees ");
+        }
+
+        public static void Customer()
+        {
+            Console.Clear();
+            Console.WriteLine("Account: ");
+            Console.WriteLine("\n1. Cart ");
+            Console.WriteLine("\n2. Items ");
+            Console.WriteLine("\n3. Settings ");
+            Console.WriteLine("\n4. Payment Details ");
         }
     }
 }
